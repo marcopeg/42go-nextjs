ff4b361e85005c9812731e512225fa6fcc420cc8
marcopeg <marco.pegoraro@gmail.com>
2025-04-11 09:24:22 +0200

    fix: improve cookies settings

--- src/lib/auth/auth-options.ts
+++ src/lib/auth/auth-options.ts
@@ -123,6 +123,34 @@ export const authOptions: NextAuthConfig = {
   session: {
     strategy: 'jwt',
   },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'None',
+        path: '/',
+        secure: true,
+      },
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        sameSite: 'None',
+        path: '/',
+        secure: true,
+      },
+    },
+    csrfToken: {
+      name: `__Host-next-auth.csrf-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'None',
+        path: '/',
+        secure: true,
+      },
+    },
+  },
   pages: {
     signIn: '/login',
     signOut: '/',
